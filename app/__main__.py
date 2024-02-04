419fecf6d2e364b7c8b037d90e3f10d0431e3e50
fastcode <fast.code.auth@gmail.com>
2024-02-04 05:09:54 +0300

    Update

--- app/__main__.py
+++ app/__main__.py
@@ -8,14 +8,5 @@ app = FastAPI(openapi_url=f"{settings.APP_API_PREFIX}/openapi.json")
 
 app.include_router(api.api_router, prefix=settings.APP_API_PREFIX)
 
-
-# TEST ACTIONS
-
-list_one = [
-    "das"
-]
-
-list_two = ["asda"]
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
