8928ec71f81e294dc67b9ec78585b2c03d808dc4
Fast Code <fast.code.auth@gmail.com>
2024-07-13 20:01:55 +0300

    fix

--- app/core/security/jwt.py
+++ app/core/security/jwt.py
@@ -1,7 +1,6 @@
 import datetime as dt
 
 import jwt
-from jwt import PyJWTError
 
 from app.core import exps
 
@@ -12,18 +11,18 @@ class JWT:
 
     def decode_token(self, token: str) -> dict | None:
         try:
-            payload = jwt.decode(token, self.secret_key, algorithms=['HS256'])
-        except PyJWTError:
+            payload = jwt.decode(token, self.secret_key, algorithms=["HS256"])
+        except Exception:
             raise exps.TOKEN_INVALID
 
-        exp = payload.get('exp')
+        exp = payload.get("exp")
         if exp and dt.datetime.now(dt.UTC).timestamp() > exp:
             raise exps.TOKEN_EXPIRED
-        return payload.get('payload')
+        return payload.get("payload")
 
     def encode_token(self, payload: dict, minutes: int) -> str:
         claims = {
-            'payload': payload,
-            'exp': dt.datetime.now(dt.UTC) + dt.timedelta(minutes=minutes),
+            "payload": payload,
+            "exp": dt.datetime.now(dt.UTC) + dt.timedelta(minutes=minutes),
         }
-        return jwt.encode(claims, self.secret_key, algorithm='HS256')
+        return jwt.encode(claims, self.secret_key, algorithm="HS256")
