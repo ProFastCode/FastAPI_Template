bce563b309c0124eb4c7989eb751554cdef308dc
Fast Code <fast.code.auth@gmail.com>
2024-07-13 20:08:52 +0300

    fix

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlmodel import SQLModel
 
-from app.core import settings
+from app.core.settings import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +24,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = SQLModel.metadata
 
-config.set_main_option('sqlalchemy.url', settings.pg_dsn.unicode_string())
+config.set_main_option("sqlalchemy.url", settings.pg_dsn.unicode_string())
 
 
 # other values from the config, defined by the needs of env.py,
